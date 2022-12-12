c8dca6536d10ee996e0bd0cd6b8d8223bffb2c88
Tea Abuselidze <teona.abus@gmail.com>
2022-12-12 14:29:13 -0600

    start

--- myweb/src/App.js
+++ myweb/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class="outlineWrapper">
+      <div class="empty"></div>
+      <div class="nameWrapper">
+      <div class="name">TEA ABUSELIDZE</div>
+      </div>
+      <div class="navWrapper">
+        <div class="nav">
+        <div class="text">
+          ABOUT ME
+        </div>
+        <div class="text">
+          PORTFOLIO
+        </div>
+        <div class="text">
+          PROCESS
+        </div>
+        </div>
+        
+      </div>
+      <div class="contactWrapper">
+        <div class="contact">CONTACT</div>
+      </div>
+      <div class="empty1"></div>
+      </div>
+
+      <div class="sec1Wrapper">
+      <div class="empty2"></div>
+      <div class="wrap0">
+      <div class="wrap1">
+
+      <div class="wrap2">
+        <div class="wrap3">
+      <div class="box1">text</div>
+      <div class="box2">text</div>
+      <div class="box3">text</div>
+      </div>
+      <div class="wrap4">
+      <div class="box4">text</div>
+      </div>
+      </div>
+
+    
+      </div>
+      <div class="box6">
+        <div class="box61">
+          text
+        </div>
+      </div>
+      </div>
+
+      <div class="vectors">
+
+      </div>
+      <div class="empty3"></div>
     </div>
+    </div>
+    
+    <div class="one">
+    </div>
+    <div class="two"></div>
+    <div class="three"></div>
+    </>
   );
 }
 
